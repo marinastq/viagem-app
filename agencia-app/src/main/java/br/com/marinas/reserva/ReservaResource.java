4d625f06cbc7c37d90412585a9b65e30491bcf6f
Marina Stanquini Santos <marinastq@gmail.com>
2023-07-14 10:26:57 -0300

    commit ReservaResource

--- agencia-app/src/main/java/br/com/marinas/reserva/ReservaResource.java
+++ agencia-app/src/main/java/br/com/marinas/reserva/ReservaResource.java
@@ -2,10 +2,15 @@ package br.com.marinas.reserva;
 
 import java.util.List;
 
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
 
 @Path("/reserva")
 public class ReservaResource {
@@ -15,5 +20,16 @@ public class ReservaResource {
     public List<Reserva> get(){
         return Reserva.listAll();
     } 
+
+    @Transactional
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response newReserva(Reserva reserva){
+        reserva.id = null;
+        reserva.persist();
+
+        return Response.status(Status.CREATED).entity(reserva).build();
+    }
 
 }
