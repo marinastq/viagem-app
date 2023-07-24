db30f3e63f35eaf2d0251243c9610c799d7080f3
Marina Stanquini Santos <marinastq@gmail.com>
2023-07-24 16:40:12 -0300

    Commit fontes micro-cliente

--- agencia-micro-cliente/src/main/resources/import.sql
+++ agencia-micro-cliente/src/main/resources/import.sql
@@ -4,18 +4,19 @@
 -- insert into myentity (id, field) values(2, 'field-2');
 -- insert into myentity (id, field) values(3, 'field-3');
 -- alter sequence myentity_seq restart with 4;
-
+/*
 CREATE SEQUENCE HIBERNATE_SEQUENCE START WITH 10 INCREMENT BY 1;
 insert into CLIENTE (id, nome) values(nextval('HIBERNATE_SEQUENCE'), 'Joao');
 insert into CLIENTE (id, nome) values(nextval('HIBERNATE_SEQUENCE'), 'Maria');
 insert into CLIENTE (id, nome) values(nextval('HIBERNATE_SEQUENCE'), 'Jose');
 insert into CLIENTE (id, nome) values(nextval('HIBERNATE_SEQUENCE'), 'Ana'); 
 insert into CLIENTE (id, nome) values(nextval('HIBERNATE_SEQUENCE'), 'Elsa');
+*/
 
-/*insert into CLIENTE (id, nome) values(1, 'Joao');
+insert into CLIENTE (id, nome) values(1, 'Joao');
 insert into CLIENTE (id, nome) values(2, 'Maria');
 insert into CLIENTE (id, nome) values(3, 'Jose');
 insert into CLIENTE (id, nome) values(4, 'Ana');
 insert into CLIENTE (id, nome) values(5, 'Elsa');
-*/
-alter sequence HIBERNATE_SEQUENCE restart with 6;
+
+alter sequence cliente_seq  restart with 6;
